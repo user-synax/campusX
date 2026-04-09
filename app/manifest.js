df4db3dbacfc9424fe9267e69c8d5a3e59a11e31
Ayush <usersynax@gmail.com>
2026-04-09 13:39:09 +0530

    feat: add PWA manifest and icons for application installation support

---
+++ app/manifest.js
@@ -0,0 +1,30 @@
+export default function manifest() {
+  return {
+    name: "CampusX",
+    short_name: "CampusX",
+    description: "The social platform exclusively for Indian college students",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0f0f0f",
+    theme_color: "#6c3bff",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
