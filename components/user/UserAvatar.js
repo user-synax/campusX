c89f6ba2e64f57a13149646f4f6ee379cae1bd92
Ayush <usersynax@gmail.com>
2026-04-13 09:12:51 +0530

    Update UserAvatar component to use standard Avatar

--- components/user/UserAvatar.js
+++ components/user/UserAvatar.js
@@ -1,20 +1,23 @@
 "use client"
 
 import { memo } from 'react'
-import AvatarWithFrame from '@/components/coins/AvatarWithFrame'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { cn } from "@/lib/utils"
 
 const UserAvatar = memo(function UserAvatar({ user, size = 'md', className }) {
-  // If the user object already has equipped data, use it
-  const equipped = user?.equipped || null
+  const sizeClasses = {
+    xs: 'h-6 w-6',
+    sm: 'h-8 w-8',
+    md: 'h-10 w-10',
+    lg: 'h-12 w-12',
+    xl: 'h-16 w-16'
+  }
 
   return (
-    <AvatarWithFrame 
-      user={user} 
-      size={size} 
-      equipped={equipped} 
-      className={className} 
-    />
+    <Avatar className={cn(sizeClasses[size] || sizeClasses.md, className)}>
+      <AvatarImage src={user?.avatar} alt={user?.name} />
+      <AvatarFallback>{user?.name?.charAt(0)?.toUpperCase()}</AvatarFallback>
+    </Avatar>
   )
 })
 
