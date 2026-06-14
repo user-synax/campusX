99b08559f86b57579e05464a63f52fa504ab7531
user-synax <usersynax@gmail.com>
2026-06-14 08:56:35 +0530

    robots.js config

--- app/robots.js
+++ app/robots.js
@@ -11,7 +11,6 @@ export default function robots() {
           '/settings',
           '/chats',
           '/notifications',
-          '/wallet',
           '/verify-student',
         ],
       },
