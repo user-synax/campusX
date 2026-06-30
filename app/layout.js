9905d10da5325445a1798e61d7ea034522a7bf98
user-synax <usersynax@gmail.com>
2026-06-30 11:14:42 +0530

    Modify layout.js for new icons and logos

--- app/layout.js
+++ app/layout.js
@@ -71,11 +71,24 @@ export const metadata = {
         },
     },
     icons: {
-        icon: "/favicon.ico",
+        icon: [
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            {
+                url: "/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                url: "/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+        ],
         shortcut: "/favicon.ico",
         apple: "/apple-touch-icon.png",
     },
-    manifest: "/manifest.webmanifest",
+    manifest: "/manifest.json",
 };
 
 export const viewport = {
